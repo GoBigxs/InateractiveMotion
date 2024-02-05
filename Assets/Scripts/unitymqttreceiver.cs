b4240beff0f8988c66131c63084de91f6dbf039d
Xu <s7000036356@senor0lunlx0425.eu.sony.com>
2024-02-05 14:30:02 +0100

    unity mqtt receiver

--- Assets/Scripts/unitymqttreceiver.cs
+++ Assets/Scripts/unitymqttreceiver.cs
@@ -10,38 +10,22 @@ using System.IO;
 using System.Linq;
 using System;
 
-[RequireComponent(typeof(CursorControlRenderOnly))]
-[RequireComponent(typeof(CursorControlRenderOnly))]
 public class UnityMqttReceiver : MonoBehaviour
 {
 
     [SerializeField]public string LocalReceiveTopic = "sony/ui";
-    [SerializeField]public string LocalReceiveTopic = "sony/ui";
-    public CursorControlRenderOnly controller;
     private String msg;
     private MqttClient client;
 
     void Awake()
     {
-        controller = GetComponent<CursorControlRenderOnly>();
-        // Server Setting 
-        // 127.0.0.1 for local server, 192.168.1.100 for on-site server
-        // create client instance 
-        client = new MqttClient(IPAddress.Parse("127.0.0.1"), 1883 , false , null );
 
-        // register to message received 
-        client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived; 
-        
-        string clientId = Guid.NewGuid().ToString(); 
-        client.Connect(clientId); 
-        
-        // subscribe to the topic "/home/temperature" with QoS 2 
-        client.Subscribe(new string[] { LocalReceiveTopic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE }); 
-        controller = GetComponent<CursorControlRenderOnly>();
         // Server Setting 
         // 127.0.0.1 for local server, 192.168.1.100 for on-site server
         // create client instance 
-        client = new MqttClient(IPAddress.Parse("127.0.0.1"), 1883 , false , null );
+        // Create a new instance of MqttClient
+	client = new MqttClient("tcp://127.0.0.1:1883");
+
 
         // register to message received 
         client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived; 
@@ -62,7 +46,6 @@ public class UnityMqttReceiver : MonoBehaviour
         {
             Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
             Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
-            controller.UpdatePos(msg);
         }
 	} 
 
